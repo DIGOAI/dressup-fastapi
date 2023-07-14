9ae7a6c77a7f3ff184cd15ab4c2a8f627160ac27
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-07-14 12:36:09 -0500

    [UPDATE] requirements.txt - config

---
+++ app/config/__init__.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+config = os.environ
--- app/middlewares/auth.py
+++ app/middlewares/auth.py
@@ -1,13 +1,12 @@
-import os
-from dotenv import load_dotenv
 from fastapi.security.api_key import APIKeyHeader
 from fastapi import Security, HTTPException
 from starlette.status import HTTP_403_FORBIDDEN
+from app.config import config
 
-load_dotenv()
-config = os.environ
+
 API_KEY = config.get("API_KEY", "")
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
+
 
 async def get_api_key(received_api_key_header: str = Security(api_key_header)):
     """Handler function to get the api key from the request header"""
