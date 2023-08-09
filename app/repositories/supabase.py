81ce46b8265698e63cf08733df1878d55d07f618
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-08-09 15:03:14 -0500

    ⚡️ [UPDATE] config file

---
+++ app/repositories/supabase.py
@@ -0,0 +1,6 @@
+from supabase import create_client, Client
+from app.config import config
+
+url: str = config.get("SUPABASE_URL")
+key: str = config.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
