4e5a2a67f21a76aaac1335c6e74014ceb6068e6e
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-17 12:30:38 -0500

    Moved patterns to their own folder.

---
+++ app/utils/__init__.py
@@ -0,0 +1,2 @@
+from .patterns import (CI_RUC_PATTERN, PASSWORD_PATTERN, PHONE_PATTERN,
+                       UUIDV4_PATTERN)
---
+++ app/utils/patterns.py
@@ -0,0 +1,4 @@
+PASSWORD_PATTERN = r"^[a-zA-Z0-9_@.-]{8,32}$"
+CI_RUC_PATTERN = r"^(0[1-9]|1\d|2[1-4])\d{8}(001)?$"
+PHONE_PATTERN = r"^(09|\+5939)\d{8}$"
+UUIDV4_PATTERN = r'^[0-9a-f]{8}-?[0-9a-f]{4}-?4[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$'
