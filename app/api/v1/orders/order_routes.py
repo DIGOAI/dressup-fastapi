defefac13501d08c38f5da755a5ea4832b267523
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-21 16:45:42 -0500

    Order complete endpoint. - Added order complete endpoint. - Added name and process_id atribute to orders schema.

--- app/api/v1/orders/order_routes.py
+++ app/api/v1/orders/order_routes.py
@@ -3,10 +3,10 @@ from fastapi import (APIRouter, Body, Depends, Form, HTTPException, Request,
 from typing_extensions import TypedDict
 
 from app.common import StorageFolder, upload_images_to_storage
-from app.middlewares import JWTBearer
+from app.middlewares import JWTBearer, Role
 from app.repositories import supabase
-from app.schemas import (Image, Order, OrderInsert, OrderUpdateStatus,
-                         OrderWithData)
+from app.schemas import (Image, ImageType, Order, OrderComplete, OrderInsert,
+                         OrderStatus, OrderUpdateStatus, OrderWithData)
 
 router = APIRouter(
     prefix="/orders", tags=["Orders"], dependencies=[Depends(JWTBearer())])
@@ -81,7 +81,39 @@ def update_order_status(request: Request, order_id: int, new_status: OrderUpdate
     return {"data": order_updated, "count": 1}
 
 
-@router.post("/new")
+@router.post("/{order_id}/complete", dependencies=[Depends(JWTBearer(min_role=Role.ADMIN))])
+def complete_order(request: Request, order_id: int, completed_order: OrderComplete = Body(...)) -> OrderResponse:
+    order_res = supabase.table("orders").select("*").eq(
+        "id", order_id).execute()
+
+    if len(order_res.data) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Order {order_id} not found.")
+
+    for image in completed_order.images:
+        image.type = ImageType.OUTPUT
+
+    images_res = supabase.table("images").insert(
+        json=[image.model_dump() for image in completed_order.images]).execute()
+
+    images_t: list[Image] = [Image(**image) for image in images_res.data]
+
+    order_images_res = supabase.table("order_items").insert(json=[{
+        "order_id": order_id,
+        "img": image.id
+    } for image in images_t]).execute()
+
+    order_res = supabase.table("orders").update(json={
+        "status": completed_order.status,
+        "metadata": completed_order.metadata or None
+    }).eq("id", order_id).execute()
+
+    order_updated = Order(**order_res.data[0])
+
+    return {"data": order_updated, "count": 1}
+
+
+@router.post("/new", dependencies=[Depends(JWTBearer(min_role=Role.ADMIN))])
 def create_order(request: Request, img_front: UploadFile, img_back: UploadFile, img_left: UploadFile, img_right: UploadFile, model: int = Form(), pose_set: int = Form()) -> OrderResponse:
     user_id = request.state.user
     role = request.state.role
