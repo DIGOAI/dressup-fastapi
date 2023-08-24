ed15cae45457a44b9d43df586bb88512abb9e808
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-24 17:45:22 -0500

    Fixed user endpoints. - Fixed bug that user/profile paths don't show up in the API.

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,6 +6,7 @@ from app.api.v1.models import model_router
 from app.api.v1.orders import order_router
 from app.api.v1.pose_sets import pose_sets_router
 from app.api.v1.poses import pose_router
+from app.api.v1.users import user_router
 from app.config import Config
 
 router = APIRouter(prefix="/v1")
@@ -29,3 +30,4 @@ router.include_router(order_router)
 router.include_router(model_router)
 router.include_router(pose_router)
 router.include_router(pose_sets_router)
+router.include_router(user_router)
